24c8b0ac5bc2b3cb15670a7a318e461d59500b2e
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2020-03-07 21:09:40 +0530

    comments added

--- smile.py
+++ smile.py
@@ -15,21 +15,27 @@ def mood(count):
         randInd = randint(0,99)
         print(f"Face {count} : \n{choice[randInd]['title']}\n{choice[randInd]['body']}\n")
 
+# Replace the following two path strings to wherever your classifier files are
 face_cascade = cv2.CascadeClassifier('/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_default.xml')
-smile_cascade = cv2.CascadeClassifier('/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_smile.xml') 
+smile_cascade = cv2.CascadeClassifier('/usr/local/lib/python3.7/site-packages/cv2/data/haarcascade_smile.xml')
+
 counter = 1
 rt = []
 cap = cv2.VideoCapture(0)
 prevface = np.array([])
 font = cv2.FONT_HERSHEY_SIMPLEX
-sm_ratio="0"
+sm_ratio = "0"
 
 try:
     while True:
+
+        # Detects faces in frame
         _, img = cap.read()
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21,21), 0) 
         faces = face_cascade.detectMultiScale(gray, 1.3, 10)
+
+        # Iterating through seen faces
         for count, (x, y, w, h) in enumerate(faces): 
 
             cv2.rectangle(img, (x, y), ((x + w), (y + h)), (255, 0, 0), 2) 
@@ -37,17 +43,23 @@ try:
 
             roi_gray = gray[y + int(2*h/3):y + h, x:x + w] 
             roi_color = img[y + int(2*h/3):y + h, x:x + w] 
+
+            # Detecting smile in each face
             smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor = 1.2,
                                                     minNeighbors = 20, minSize = (25, 25)) 
       
+            # Iterating through smiles in each face (lol)
             for (sx, sy, sw, sh) in smiles: 
                 sm_ratio = str(round(sw/sx, 3))
                 cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 0, 255), 2) 
                 if float(sm_ratio) > 1.8:
                     cv2.rectangle(roi_color, (sx, sy), ((sx + sw), (sy + sh)), (0, 255, 0), 2) 
 
+        # show frame with all rectangles drawn
         cv2.imshow('Smile Detector', img)
         k = cv2.waitKey(30) & 0xff
+
+        # Handles entry and exit of multiple faces in an out of frame
         if len(prevface) > len(faces):
             for count,item in enumerate(prevface):
                 fil = list(filter(lambda x:abs(x[0] - item[0]) < 100, faces))
@@ -63,11 +75,14 @@ try:
                 rt[-1].run()
                 counter += 1
         prevface = faces
+
+        # Exits if escape is pressed
         if k == 27:
             raise Exception('Pressed escape key')
-except:
+except :
     print("\nbabye!\n")
 
+# Just quits everything (as I soon will)
 for i in rt:
     i.stop()
 cap.release()
