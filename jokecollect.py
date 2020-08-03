290f74b5b5f2f1bcc1c6ab64255dfa41a89c7402
Kaushik Sivashankar <kaushik.sivashankar@gmail.com>
2020-08-03 11:43:33 +0530

    Added requirements

--- jokecollect.py
+++
@@ -1,5 +0,0 @@
-import pandas
-import json
-best = json.load(open("/Users/Kaushik/programs/smile/reddit_jokes.json"))
-top = sorted(best, key = lambda x:x["score"])
-json.dump(top[-100:], open("best.json", "w"), indent = 4)
